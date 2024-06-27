a2cfe8e6f862921e99082c7cdd48e59eaddf8900
cornerbytes <cornerbytes@protonmail.com>
2024-06-27 17:03:06 +0700

    add rev/micro_rev

---
+++ 2024/PLAYGROUND_TCP1P/reversing/micro_rev/solver.py
@@ -0,0 +1,13 @@
+key = ["22","11","75","e1","66","12","0a","75","e1","66"]
+
+if __name__ == "__main__":
+    key = [int(i, 16) for i in key]
+
+    with open('enc.txt', 'rb') as f:
+        enc_f = f.read()[:-1] # remove last byte
+        enc_f = list(enc_f)
+
+    flag = ''
+    for i in range(len(enc_f)):
+        flag += chr(enc_f[i] ^ key[i % len(key)])
+    print(f'here flag : {flag}')
